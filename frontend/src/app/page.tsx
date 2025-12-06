76509aefd7e22b9ab9f97951f73bac8311de7ec2
javad-kuttappi <javaadde123@hotmail.com>
2025-12-06 10:44:47 +0530

    added frontend

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Navabar } from "@/components/layout/Navabar";
+
+export default function Home() {
+  return (
+   <div className="min-h-screen p-7 bg-[#EEF0F4]">
+    <Navabar />
+    {/* <h1>Welcome to the Home Page</h1> */}
+   </div>
+  );
+}
