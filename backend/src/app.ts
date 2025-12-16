032f46e0134087e001163b534b2b417fc76dfbe4
Javad <javaadde123@hotmail.com>
2025-12-16 20:44:49 +0530

    added security features like morgan and devlopment features like morgan

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,15 +2,18 @@ import express, { Application } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config();
 import connectDB from "./config/database";
 import authRoutes from "./routes/auth.routes";
 import paymentRoutes from "./routes/payment.routes";
 import passport from "./config/passport";
 import { notFoundHandler, serverErrorHandler } from "./middleware/error.middleware";
 import { ENV } from "./config/env";
+import helmet from "helmet";
+import { SuccessResponse } from "./utils/responses";
+import morgan from "morgan";
 
 
+dotenv.config();
 const expressInstance: Application = express();
 
 connectDB();
@@ -21,24 +24,17 @@ const corsOptions = {
 };
 
 // TODO: use windston and morgan.
-// TODO: create a single file to import all envs,
 
+expressInstance.use(morgan("dev"));
 expressInstance.use(cors(corsOptions));
-
-// TODO: use helmet
-
-
+expressInstance.use(helmet());  
 expressInstance.use(cookieParser());
 expressInstance.use(express.json());
 expressInstance.use(express.urlencoded({ extended: true }));
 expressInstance.use(passport.initialize());
 
 expressInstance.get("/health", (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: "Server is running",
-    timestamp: new Date().toISOString(),
-  });
+  new SuccessResponse("Server is running").send(res);
 });
 
 const appRouter = express.Router();
