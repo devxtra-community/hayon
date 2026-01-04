503db55e00cb99ad4d431bc20dad8b39a064e5fe
hafzism <hafeezpallath@gmail.com>
2026-01-04 23:56:19 +0530

    chore(env): renamed frontend envs

--- frontend/src/components/BackButton.tsx
+++ frontend/src/components/BackButton.tsx
@@ -4,12 +4,10 @@ import { ArrowLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
 export function BackButton() {
   const router = useRouter();
-  return       <Button
-          variant="outline"
-          className="absolute top-8 left-8 z-20"
-          onClick={() => router.back()}
-        >
-          <ArrowLeft className="w-4 h-4 mr-2" />
-          Go Back
-        </Button>;
+  return (
+    <Button variant="outline" className="absolute top-8 left-8 z-20" onClick={() => router.back()}>
+      <ArrowLeft className="w-4 h-4 mr-2" />
+      Go Backey
+    </Button>
+  );
 }
