2c14914491b17497ffeaa6e0598a6214af652f91
hafzism <hafeezpallath@gmail.com>
2026-02-08 13:36:24 +0530

    refactor(components): create index.ts to simplify imports

---
+++ frontend/src/components/analytics/index.ts
@@ -0,0 +1,7 @@
+export { default as EngagementTimeline } from "./EngagementTimeline";
+export { default as PlatformMetricsCard } from "./PlatformMetricsCard";
+export { default as FollowersPieChart } from "./FollowersPieChart";
+export { default as TopPerformingPostCard } from "./TopPerformingPostCard";
+export { default as GrowthChart } from "./GrowthChart";
+export { default as AnalyticsEngagementChart } from "./AnalyticsEngagementChart";
+export { default as AnalyticsInsightsCard } from "./AnalyticsInsightsCard";
